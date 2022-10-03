2b061c9e4af8b57a2c330429caa3224a09a773cf
ammujayaraman6@gmail.com <sureshthirusr@gmail.com>
2022-10-03 13:58:01 +0530

    TC 4 completed

--- src/test/java/com/makemytrip/runner/RunWithChrome.java
+++ src/test/java/com/makemytrip/runner/RunWithChrome.java
@@ -7,7 +7,7 @@ import org.junit.runner.RunWith;
 import org.openqa.selenium.WebDriver;
 import com.makemytrip.baseclass.BaseClass;
 import cucumber.api.CucumberOptions;
-import cucumber.api.junit.Cucumber
+import cucumber.api.junit.Cucumber;
 
 
 @RunWith(Cucumber.class)
